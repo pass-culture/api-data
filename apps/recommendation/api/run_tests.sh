00b7b3081642c352e4980b2f73de4fe4f20d2a92
anismoula123 <125356245+anismoula123@users.noreply.github.com>
2023-08-22 11:34:58 +0200

    Update run_tests.sh

--- apps/recommendation/api/run_tests.sh
+++ apps/recommendation/api/run_tests.sh
@@ -11,6 +11,7 @@ fi
 
 [ "$CI" '!=' true ] && docker-compose up -d testdb
 function wait_for_container () {(
+    echo "Debug :  executre wait_for_container"
     until PGPASSWORD=postgres psql -h postgres -p $DATA_GCP_TEST_POSTGRES_PORT -U "postgres" -c '\q'; do
       >&2 echo "Postgres is unavailable - sleeping"
       sleep 2
@@ -22,6 +23,7 @@ function run () {(
 sleep 3
 wait_for_container
 run
+echo "Debug :  execute RUN"
 status=$?
 
 [ "$CI" '!=' true ] && docker-compose stop testdb && docker-compose rm -f testdb
