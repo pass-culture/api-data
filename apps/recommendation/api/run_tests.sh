994066011ca2b6790d5e3e8261a0dbddfb72a1e1
anismoula123 <125356245+anismoula123@users.noreply.github.com>
2023-09-08 14:11:07 +0200

    PC-23878 PC-24091  Setup Data CICD  ( RECO & Compliance ) (#13)

--- apps/recommendation/api/run_tests.sh
+++ apps/recommendation/api/run_tests.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+export DATA_GCP_TEST_POSTGRES_PORT=5432
+export DB_NAME="db"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-if [ "$CI" '=' true ]
-then
-  export DATA_GCP_TEST_POSTGRES_PORT=5432
-  export DB_NAME="db"
-else
-  set +a; source ../../.env.local; set -a;
-fi
+export PG_HOST="${PG_HOST:-localhost}"
 
 [ "$CI" '!=' true ] && docker-compose up -d testdb
 function wait_for_container () {(
-    echo "Debug :  executre wait_for_container"
-    until PGPASSWORD=postgres psql -h postgres -p $DATA_GCP_TEST_POSTGRES_PORT -U "postgres" -c '\q'; do
+    until PGPASSWORD=postgres psql -h $PG_HOST -p $DATA_GCP_TEST_POSTGRES_PORT -U "postgres" -c '\q'; do
       >&2 echo "Postgres is unavailable - sleeping"
       sleep 2
     done
