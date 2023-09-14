33770453b9b47ad9456a80d7b6b29d826e708d79
LucileRainteau <lucile.rainteau@passculture.app>
2023-09-14 18:15:20 +0200

    apply linter

--- apps/recommendation/api/src/huggy/utils/env_vars.py
+++ apps/recommendation/api/src/huggy/utils/env_vars.py
@@ -29,7 +29,7 @@ try:
     SQL_BASE_PASSWORD = access_secret(GCP_PROJECT, SQL_BASE_SECRET_ID)
 except:
     SQL_BASE_PASSWORD = os.environ.get("SQL_BASE_PASSWORD", "postgres")
-    
+
 SQL_PORT = os.environ.get("SQL_PORT")
 SQL_HOST = os.environ.get("SQL_HOST")
 # logger
