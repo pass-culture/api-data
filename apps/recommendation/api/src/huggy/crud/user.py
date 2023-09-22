e17dc436bdf65675e13bacce7a272fa664bf605d
LucileRainteau <lucile.rainteau@passculture.app>
2023-09-22 11:46:01 +0200

    Get available table for user

--- apps/recommendation/api/src/huggy/crud/user.py
+++ apps/recommendation/api/src/huggy/crud/user.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func
 
-from huggy.models import enriched_user
 from huggy.schemas.user import User
 from huggy.crud.iris import get_iris_from_coordinates
+from huggy.models.enriched_user import get_available_table
+from huggy.utils.database import bind_engine
 
 
 def get_user_profile(
@@ -13,19 +14,21 @@ def get_user_profile(
     an user. (age, number of bookings, number of clicks, number of favorites,
     amount of remaining deposit).
     """
+
+    user_table = get_available_table(bind_engine, "User")
+
     user_profile = (
         db.query(
-            enriched_user.User.user_deposit_creation_date
-            - enriched_user.User.user_birth_date,
-            func.coalesce(enriched_user.User.booking_cnt, 0),
-            func.coalesce(enriched_user.User.consult_offer, 0),
-            func.coalesce(enriched_user.User.has_added_offer_to_favorites, 0),
+            user_table.user_deposit_creation_date - user_table.user_birth_date,
+            func.coalesce(user_table.booking_cnt, 0),
+            func.coalesce(user_table.consult_offer, 0),
+            func.coalesce(user_table.has_added_offer_to_favorites, 0),
             func.coalesce(
-                enriched_user.User.user_theoretical_remaining_credit,
-                enriched_user.User.user_deposit_initial_amount,
+                user_table.user_theoretical_remaining_credit,
+                user_table.user_deposit_initial_amount,
             ),
         )
-        .filter(enriched_user.User.user_id == user_id)
+        .filter(user_table.user_id == user_id)
         .first()
     )
 
--- apps/recommendation/api/src/huggy/models/enriched_user.py
+++ apps/recommendation/api/src/huggy/models/enriched_user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, DateTime, Float
+from sqlalchemy import Column, String, Integer, DateTime, Float, ForeignKey, inspect
 
 from huggy.utils.database import Base
 
@@ -16,3 +16,27 @@ class User(Base):
     booking_cnt = Column(Integer)
     consult_offer = Column(Integer)
     has_added_offer_to_favorites = Column(Integer)
+
+
+class UserMv(User):
+    """Database model of enriched_user table.
+    This table is used to get informations about the user calling the API."""
+
+    __tablename__ = "enriched_user_mv"
+    user_id = Column(None, ForeignKey("enriched_user.user_id"), primary_key=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "mv",
+        "inherit_condition": (user_id == User.user_id),
+    }
+
+
+def get_available_table(engine, model_base) -> str:
+    for suffix in ["", "Mv", "MvTmp", "MvOld"]:
+        model = f"{model_base}{suffix}"
+        table_name = eval(model).__tablename__
+        result = inspect(engine).has_table(table_name)
+        if result:
+            return eval(model)
+        else:
+            return "no table found"
