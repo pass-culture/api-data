cf175aff49c93f7b5c45b2c3312228e234ff4505
cdelabre <cyrille.delabre@passculture.app>
2023-11-03 17:01:22 +0100

    (reco) feat: add gtl endpoints

--- apps/recommendation/api/src/huggy/core/model_selection/recommendation.py
+++ apps/recommendation/api/src/huggy/core/model_selection/recommendation.py
@@ -4,6 +4,7 @@ import huggy.core.scorer.offer as offer_scorer
 from huggy.core.model_selection.model_configuration import (
     ModelConfiguration,
     diversification_on,
+    DiversificationParams,
 )
 
 retrieval_filter = ModelConfiguration(
@@ -12,6 +13,42 @@ retrieval_filter = ModelConfiguration(
     scorer=offer_scorer.OfferScorer,
     diversification_params=diversification_on,
     retrieval_endpoints=[user_retrieval.filter_retrieval_endpoint],
+    ranking_endpoint=user_ranking.user_ranking_endpoint,
+)
+
+
+gtl_filter = ModelConfiguration(
+    name="recommendation_filter",
+    description="""""",
+    scorer=offer_scorer.OfferScorer,
+    diversification_params=DiversificationParams(
+        is_active=True,
+        is_reco_shuffled=True,
+        mixing_features="search_group_name",
+        order_column="offer_score",
+        order_ascending=False,
+        submixing_feature_dict={"LIVRE_PAPIER": "gtl_id"},
+    ),
+    retrieval_endpoints=[user_retrieval.filter_retrieval_endpoint],
+    ranking_endpoint=user_ranking.user_ranking_endpoint,
+)
+
+gtl_reco = ModelConfiguration(
+    name="recommendation_user",
+    description="""""",
+    scorer=offer_scorer.OfferScorer,
+    diversification_params=DiversificationParams(
+        is_active=True,
+        is_reco_shuffled=True,
+        mixing_features="search_group_name",
+        order_column="offer_score",
+        order_ascending=False,
+        submixing_feature_dict={"LIVRE_PAPIER": "gtl_id"},
+    ),
+    retrieval_endpoints=[
+        user_retrieval.filter_retrieval_endpoint,
+        user_retrieval.recommendation_retrieval_endpoint,
+    ],
     ranking_endpoint=user_ranking.user_ranking_endpoint,
 )
 
@@ -27,6 +64,7 @@ retrieval_reco = ModelConfiguration(
     ],
     ranking_endpoint=user_ranking.user_ranking_endpoint,
 )
+
 
 retrieval_filter_version_b = ModelConfiguration(
     name="recommendation_filter",
