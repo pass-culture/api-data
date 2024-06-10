8452757f6f954dbd536bf0241e29c05723b3e4a7
Laurent Montier <laurent.montier@passculture.app>
2024-06-10 11:51:22 +0200

    Move compliance_loader to core/compliance/loaders.py

--- apps/fraud/compliance/api/src/pcpapillon/views/compliance.py
+++ apps/fraud/compliance/api/src/pcpapillon/views/compliance.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
 from fastapi_versioning import version
 from main import custom_logger, setup_trace
+from pcpapillon.core.compliance.loaders import load_config, load_models
 from pcpapillon.core.compliance.predict import get_prediction_and_main_contribution
 from pcpapillon.core.compliance.preprocess import preprocess
-from pcpapillon.utils.compliance_loaders import load_config, load_models
 from pcpapillon.utils.config_handler import ConfigHandler
 from pcpapillon.utils.data_model import (
     ComplianceInput,
