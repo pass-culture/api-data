131c386a17bab8deed9ff78bb1fd6384549277d2
Laurent Montier <laurent.montier@passculture.app>
2024-06-14 08:41:47 +0200

    WIP: refacto logger

--- apps/fraud/compliance/api/src/pcpapillon/utils/cloud_logging/setup.py
+++ apps/fraud/compliance/api/src/pcpapillon/utils/cloud_logging/setup.py
@@ -1,21 +1,45 @@
 import logging
 
-import google.cloud.logging
 from fastapi import Request
 from fastapi.logger import logger
+from fastapi.logger import logger as fastapi_logger
+from google.cloud import logging as gcloud_logging
 from pcpapillon.utils.cloud_logging.filter import GoogleCloudLogFilter
-from pcpapillon.utils.cloud_logging.logger import CustomLogger
 from pcpapillon.utils.env_vars import (
     cloud_trace_context,
     isAPI_LOCAL,
 )
 
 
+class CustomLogger:
+    def info(self, message=None, extra=None):
+        log_entry = {
+            "message": message,
+            "extra": extra,
+        }
+        fastapi_logger.info(log_entry)
+        return
+
+
 def setup_logging():
+    # Clear existing handlers
+    fastapi_logger.handlers = []
+
     if isAPI_LOCAL:
-        api_logger = logging.getLogger("uvicorn")
+        # Local development with Uvicorn or Gunicorn with Uvicorn workers
+        # uvicorn_logger = logging.getLogger("uvicorn")
+        # uvicorn_logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setFormatter(
+            logging.Formatter("%(levelname)s: %(asctime)s - %(message)s")
+        )
+        # uvicorn_logger.addHandler(handler)
+        fastapi_logger.addHandler(handler)
+        fastapi_logger.setLevel(logging.DEBUG)
+        api_logger = fastapi_logger
     else:
-        client = google.cloud.logging.Client()
+        # Production on Cloud Run
+        client = gcloud_logging.Client()
         handler = client.get_default_handler()
         handler.setLevel(logging.DEBUG)
         handler.filters = []
