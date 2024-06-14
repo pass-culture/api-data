4841486e284fcc1f1e5d89ad46122cbc050f152b
Laurent Montier <laurent.montier@passculture.app>
2024-06-14 08:02:46 +0200

    Add start_as_not_local

--- apps/fraud/compliance/api/src/pcpapillon/utils/tools.py
+++ apps/fraud/compliance/api/src/pcpapillon/utils/tools.py
@@ -1,12 +1,14 @@
 import os
-
-import mlflow
 from google.auth.transport.requests import Request
 from google.oauth2 import id_token
+import mlflow
 from pcpapillon.utils.env_vars import MLFLOW_URL
 
 
 def connect_remote_mlflow(client_id):
-    os.environ["MLFLOW_TRACKING_TOKEN"] = id_token.fetch_id_token(Request(), client_id)
+    if "MLFLOW_TRACKING_TOKEN" not in os.environ:
+        os.environ["MLFLOW_TRACKING_TOKEN"] = id_token.fetch_id_token(
+            Request(), client_id
+        )
     uri = MLFLOW_URL
     mlflow.set_tracking_uri(uri)
