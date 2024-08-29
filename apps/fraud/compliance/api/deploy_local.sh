cf21751476cdb62e627f55653ce2d3f8fdcf3631
LaurentM Pass <laurent.montier@passculture.app>
2024-08-29 09:47:38 +0200

    feat: Update Dockerfile so that it can run locally

--- apps/fraud/compliance/api/deploy_local.sh
+++ apps/fraud/compliance/api/deploy_local.sh
@@ -1,2 +1,7 @@
-docker build -t pcpapillon --progress=plain .
-docker run -it -p 8080:8080 pcpapillon
+if [ ! -f ~/.config/gcloud/application_default_credentials.json ]; then
+    gcloud auth application-default login
+fi
+cp ~/.config/gcloud/application_default_credentials.json src/application_default_credentials.json
+docker build -t pcpapillon --build-arg LOCAL=true --build-arg GOOGLE_CLOUD_PROJECT=passculture-data-ehp .
+rm src/application_default_credentials.json
+docker run -p 8080:8080 -e "PORT=8080" pcpapillon
