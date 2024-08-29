0efe8ba105ed554c4c28a7dd6e6207f88a9a565c
LaurentM Pass <laurent.montier@passculture.app>
2024-08-29 17:05:45 +0200

    refactor: Remove label data

--- apps/fraud/compliance/api/src/pcpapillon/core/offer_categorisation_model.py
+++ apps/fraud/compliance/api/src/pcpapillon/core/offer_categorisation_model.py
@@ -1,7 +1,3 @@
-import time
-
-import numpy as np
-import pandas as pd
 from main import custom_logger
 from pcpapillon.utils.constants import (
     ModelName,
@@ -14,7 +10,6 @@ from pcpapillon.utils.model_handler import ModelHandler, ModelWithMetadata
 
 
 class OfferCategorisationModel:
-    LABEL_MAPPING_PATH = "pcpapillon/data/offer_categorisation_label_mapping.parquet"  # Will be removed when model predict is updated
     MODEL_NAME = ModelName.OFFER_CATEGORISATION
 
     def __init__(self):
@@ -36,41 +31,9 @@ class OfferCategorisationModel:
         predictions = self.model.predict(data.dict())
         print(predictions)
         return predictions
-        # return OfferCategorisationOutput(
-        #     **pd.DataFrame(
-        #         {
-        #             "subcategory": top_categories,
-        #             "probability": probabilities[top_indexes],
-        #         }
-        #     ).to_dict(orient="records")
-        # )
-
-    def _postprocess(
-        self,
-        probabilities: pd.Series,
-        n_top: int,
-    ):
-        t0 = time.time()
-
-        top_indexes = probabilities.argsort()[-n_top:][::-1]
-        top_categories = self.classes_to_label_mapping.iloc[top_indexes]
-
-        custom_logger.debug(f"elapsed time for postprocessing {time.time() - t0}")
-
-        return pd.DataFrame(
-            {
-                "subcategory": top_categories,
-                "probability": probabilities[top_indexes],
-            }
-        ).to_dict(orient="records")
 
     def _load_models(self) -> ModelWithMetadata:
         custom_logger.info("Load offer categorisation model..")
         return self.model_handler.get_model_with_metadata_by_name(
             model_name=self.MODEL_NAME
         )
-
-    @classmethod
-    def _load_classes_to_label_mapping(cls, model_classes: np.ndarray) -> pd.Series:
-        label_mapping = pd.read_parquet(cls.LABEL_MAPPING_PATH)
-        return label_mapping.iloc[model_classes]["offer_subcategoryId"]
