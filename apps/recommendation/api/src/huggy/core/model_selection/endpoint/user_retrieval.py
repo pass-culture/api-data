61b87134bff5a66f7beaa96b9fab6547e167d4cd
cdelabre <103637148+cdelabre@users.noreply.github.com>
2024-10-11 17:26:43 +0200

    [AB-42](ab-test) Update config for retrieval version b endpoint (#231)

--- apps/recommendation/api/src/huggy/core/model_selection/endpoint/user_retrieval.py
+++ apps/recommendation/api/src/huggy/core/model_selection/endpoint/user_retrieval.py
@@ -6,6 +6,7 @@ from huggy.core.endpoint.retrieval_endpoint import (
 )
 from huggy.core.model_selection.endpoint import RetrievalEndpointName
 
+# default
 filter_retrieval_endpoint = FilterRetrievalEndpoint(
     endpoint_name=RetrievalEndpointName.recommendation_user_retrieval,
     size=150,
@@ -18,16 +19,39 @@ recommendation_retrieval_endpoint = RecommendationRetrievalEndpoint(
     use_cache=True,
 )
 
-
 trend_release_date_retrieval_endpoint = ReleaseTrendRetrievalEndpoint(
     endpoint_name=RetrievalEndpointName.recommendation_user_retrieval,
     size=150,
     use_cache=True,
 )
 
-
 trend_creation_date_retrieval_endpoint = CreationTrendRetrievalEndpoint(
     endpoint_name=RetrievalEndpointName.recommendation_user_retrieval,
+    size=150,
+    use_cache=True,
+)
+
+# version B
+filter_retrieval_endpoint_version_b = FilterRetrievalEndpoint(
+    endpoint_name=RetrievalEndpointName.recommendation_user_retrieval_version_b,
+    size=150,
+    use_cache=True,
+)
+
+recommendation_retrieval_endpoint_version_b = RecommendationRetrievalEndpoint(
+    endpoint_name=RetrievalEndpointName.recommendation_user_retrieval_version_b,
+    size=150,
+    use_cache=True,
+)
+
+trend_release_date_retrieval_endpoint_version_b = ReleaseTrendRetrievalEndpoint(
+    endpoint_name=RetrievalEndpointName.recommendation_user_retrieval_version_b,
+    size=150,
+    use_cache=True,
+)
+
+trend_creation_date_retrieval_endpoint_version_b = CreationTrendRetrievalEndpoint(
+    endpoint_name=RetrievalEndpointName.recommendation_user_retrieval_version_b,
     size=150,
     use_cache=True,
 )
