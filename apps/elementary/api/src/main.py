b862aed953e870107195ac82a364b0c3d133558b
vbusson-pass <142802218+vbusson-pass@users.noreply.github.com>
2025-02-14 09:18:36 +0100

    (BSR) elementary deployment (#294)
    * bsr: migrate elementary from data-apps repo
    
    * clean dockerfile
    
    * chore: enhance makefile
    
    * feat: elementary deployment in ci
    
    * review

---
+++ apps/elementary/api/src/main.py
@@ -0,0 +1,21 @@
+from constants import DATA_BUCKET_NAME
+from flask import Flask
+from utils import download_blob, get_latest_file
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def serve_static_file():
+    report_file = get_latest_file(DATA_BUCKET_NAME)
+    html_content = download_blob(DATA_BUCKET_NAME, report_file)
+    return html_content
+
+
+@app.route("/health")
+def check_health():
+    return "OK", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=False, port=8081)
