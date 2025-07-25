1ab4e52eeef665a3b82e551831e9ab2b19723fae
Louis GERARD <186476141+lgerard-pass@users.noreply.github.com>
2025-07-25 16:14:51 +0200

    WIP

--- apps/fraud/compliance/api/src/pcpapillon/utils/security.py
+++ apps/fraud/compliance/api/src/pcpapillon/utils/security.py
@@ -38,31 +38,3 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     # Add your encoding logic here, such as signing the token with a secret key
     return to_encode
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[HASH_ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
