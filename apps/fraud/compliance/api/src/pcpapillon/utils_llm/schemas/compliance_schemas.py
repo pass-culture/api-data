43c604020055a81e70400851666160d27a7761fd
Louise Rigal <201611893+lrigal-pass@users.noreply.github.com>
2025-08-05 16:55:15 +0200

    WIP

--- apps/fraud/compliance/api/src/pcpapillon/utils_llm/schemas/compliance_schemas.py
+++ apps/fraud/compliance/api/src/pcpapillon/utils_llm/schemas/compliance_schemas.py
@@ -6,17 +6,17 @@ COMPLIANCE_SCHEMAS = {
             "name": "reponse_LLM",
             "description": """Classification finale de l'offre commerciale
             selon les règles de conformité.
-            Les valeurs possibles sont 'ACCEPTED', si l'offre répond à toutes les règles
-            de conformité, 'REJECTED', si elle enfreint au moins une règle et
-            'UNDETERMINED' s'il y a le moindre doute.
-            En cas d'incertitude, il est préférable de classifier en UNDETERMINED plutôt
+            Les valeurs possibles sont 'approved', si l'offre répond à toutes les règles
+            de conformité, 'rejected', si elle enfreint au moins une règle et
+            'undetermined' s'il y a le moindre doute.
+            En cas d'incertitude, il est préférable de classifier en undetermined plutôt
             que de risquer une mauvaise décision.""",
             "type": "string",
         },
         {
             "name": "explication_classification",
             "description": """Explication détaillée de la raison pour laquelle l'offre a
-            été classée comme ACCEPTED, REJECTED ou UNDETERMINED. Doit spécifiquement
+            été classée comme approved, rejected ou undetermined. Doit spécifiquement
             faire référence à la ou les règles de conformité concernées et expliquer
             comment l'offre s'y rapporte.""",
             "type": "string",
@@ -38,7 +38,7 @@ COMPLIANCE_SCHEMAS = {
         {
             "name": "points_amelioration",
             "description": """Suggestions concises pour améliorer la conformité de
-            l'offre si elle est classée comme 'REJECTED'. Proposer des actions
+            l'offre si elle est classée comme 'rejected'. Proposer des actions
             spécifiques que l'acteur culturel pourrait entreprendre pour rendre son
             offre conforme. 'Non applicable' si l'offre est conforme.""",
             "type": "string",
