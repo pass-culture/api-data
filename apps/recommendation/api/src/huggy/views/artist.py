0616a2c16fee2d658e522fe2721967666904a6ca
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-03-17 14:34:58 +0100

    pr: Take simple gemini returns

--- apps/recommendation/api/src/huggy/views/artist.py
+++ apps/recommendation/api/src/huggy/views/artist.py
@@ -1,34 +1,13 @@
 import typing as t
 
 from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from huggy.crud.artist import Artist
+from huggy.crud.artist import get_similar_artists_from_db
 from huggy.database.session import get_db
 from huggy.views.common import check_token, get_call_id, setup_trace
 
 artist_router = r = APIRouter(tags=["artists"])
-
-
-async def __similar_artists(
-    db: AsyncSession,
-    artist_id: str,
-    call_id: str,
-):
-    similar_artists = await Artist.get_similar_artists_from_db(
-        db=db, artist_id=artist_id
-    )
-
-    return jsonable_encoder(
-        {
-            "similar_artists": similar_artists,
-            "params": {
-                "artist_id": artist_id,
-                "call_id": call_id,
-            },
-        }
-    )
 
 
 @r.get(
@@ -41,8 +20,12 @@ async def similar_artists(
     db: AsyncSession = Depends(get_db),
     call_id: str = Depends(get_call_id),
 ):
-    return await __similar_artists(
-        db,
-        artist_id=artist_id,
-        call_id=call_id,
-    )
+    similar_artists = await get_similar_artists_from_db(db=db, artist_id=artist_id)
+
+    return {
+        "similar_artists": similar_artists,
+        "params": {
+            "artist_id": artist_id,
+            "call_id": call_id,
+        },
+    }
