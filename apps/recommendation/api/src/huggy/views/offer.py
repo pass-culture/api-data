eadfeed20ab0953165a585bfc693fad5b0cc7c7d
LaurentM Pass <172769973+lmontier-pass@users.noreply.github.com>
2026-03-25 13:42:52 +0100

    refactor: Remove useless function in get_similar_offers

--- apps/recommendation/api/src/huggy/views/offer.py
+++ apps/recommendation/api/src/huggy/views/offer.py
@@ -14,17 +14,29 @@ from huggy.views.common import check_token, get_call_id, setup_trace
 offer_router = r = APIRouter(tags=["offer"])
 
 
-async def __similar_offers(
-    db: AsyncSession,
+@r.get(
+    "/similar_offers/{offer_id}",
+    dependencies=[Depends(setup_trace), Depends(check_token)],
+)
+async def get_similar_offers(
     offer_id: str,
-    playlist_params: p.PlaylistParams,
-    latitude: t.Optional[float],
-    longitude: t.Optional[float],
-    call_id: str,
+    token: t.Optional[str] = None,
+    latitude: t.Optional[float] = None,
+    longitude: t.Optional[float] = None,
+    user_id: t.Optional[str] = None,
+    categories: t.Optional[list[str]] = Query(None),
+    subcategories: t.Optional[list[str]] = Query(None),
+    search_group_names: t.Optional[list[str]] = Query(None),
+    db: AsyncSession = Depends(get_db),
+    call_id: str = Depends(get_call_id),
 ):
-    # legacy: include main offer_id in the list of offers
-    playlist_params.add_offer(offer_id)
-
+    playlist_params = p.GetSimilarOfferPlaylistParams(
+        user_id=user_id,
+        categories=categories,
+        subcategories=subcategories,
+        search_group_names=search_group_names,
+        input_offers=[offer_id],
+    )
     user = await UserContextDB().get_user_context(
         db, playlist_params.user_id, latitude, longitude
     )
@@ -49,37 +61,4 @@ async def __similar_offers(
                 "call_id": call_id,
             },
         }
-    )
-
-
-@r.get(
-    "/similar_offers/{offer_id}",
-    dependencies=[Depends(setup_trace), Depends(check_token)],
-)
-async def get_similar_offers(
-    offer_id: str,
-    token: t.Optional[str] = None,
-    latitude: t.Optional[float] = None,
-    longitude: t.Optional[float] = None,
-    user_id: t.Optional[str] = None,
-    categories: t.Optional[list[str]] = Query(None),
-    subcategories: t.Optional[list[str]] = Query(None),
-    search_group_names: t.Optional[list[str]] = Query(None),
-    db: AsyncSession = Depends(get_db),
-    call_id: str = Depends(get_call_id),
-):
-    playlist_params = p.GetSimilarOfferPlaylistParams(
-        user_id=user_id,
-        categories=categories,
-        subcategories=subcategories,
-        search_group_names=search_group_names,
-    )
-
-    return await __similar_offers(
-        db,
-        offer_id=offer_id,
-        playlist_params=playlist_params,
-        latitude=latitude,
-        longitude=longitude,
-        call_id=call_id,
     )
