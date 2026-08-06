742964b8939c476a708d2ebc680a8245792e6bba
dcuesta-pass <82374920+dcuesta-pass@users.noreply.github.com>
2026-08-06 15:19:37 +0200

    Update apps/fraud/compliance/api/src/pcpapillon/views/search_edito.py
    Co-authored-by: jmontagnat-pass <201599413+jmontagnat-pass@users.noreply.github.com>

--- apps/fraud/compliance/api/src/pcpapillon/views/search_edito.py
+++ apps/fraud/compliance/api/src/pcpapillon/views/search_edito.py
@@ -52,4 +52,5 @@ def search_edito(
         endpoint_resource_name=SEARCH_EDITO_MODEL_ENDPOINT_NAME,
         instances=[payload],
     )
-    return SearchEditoOutput(results=predictions)
+)
+return SearchEditoOutput(results=predictions or [])
