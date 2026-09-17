c46feaf07b0457bef13169482f81b948cb57b1a6
jmontagnat-pass <201599413+jmontagnat-pass@users.noreply.github.com>
2026-09-17 12:23:27 +0200

    (BSR): fix(reco_v2): align SQLAlchemy model field declarations and table names with the actual database schema (#585)

--- apps/recommendation_v2/src/models/iris.py
+++ apps/recommendation_v2/src/models/iris.py
@@ -14,6 +14,8 @@ class IrisFrance(Base):
 
     id: sa_orm.Mapped[str] = sa_orm.mapped_column(String(256), primary_key=True)
 
-    centroid: sa_orm.Mapped[Any] = sa_orm.mapped_column(Geography("POINT", srid=4326), nullable=True)
     iriscode: sa_orm.Mapped[int] = sa_orm.mapped_column(Integer)
-    shape: sa_orm.Mapped[Any] = sa_orm.mapped_column(Geometry("POLYGON"))
+    centroid: sa_orm.Mapped[Any | None] = sa_orm.mapped_column(
+        Geography(geometry_type="POINT", srid=4326), nullable=True
+    )
+    shape: sa_orm.Mapped[Any] = sa_orm.mapped_column(Geometry("POLYGON", srid=0))
